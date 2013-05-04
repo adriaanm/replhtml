5cbbe50cd25953618ea66eba6241977e2eeeb180
Jason Zaugg <jzaugg@gmail.com>
2013-05-04 18:22:30 +0200

    Watch more files.

--- src/main/scala/Keydown.scala
+++ src/main/scala/Keydown.scala
@@ -8,7 +8,8 @@ class Keydown(val root: File) {
   def /(path: String) = new File(root, path)
 
   def regenerateSlidesIfNeeded() {
-    val sourceModified = (this / "slides.md").lastModified()
+    val sources = Seq(this / "slides.md") ++ (Seq("css", "js", "images").flatMap(p => (this / p).listFiles()))
+    val sourceModified = sources.map(_.lastModified()).max
     val outputModified = Try((this / "slides.html").lastModified()).getOrElse(0L)
     if (sourceModified > outputModified) {
       val process = sys.process.Process(Seq("keydown", "slides", "slides.md"), root)
